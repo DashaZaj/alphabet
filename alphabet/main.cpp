1b1f1903e5bbe45150c92a732118f25579d818ef
Dasha_Zajc <no@no.no>
2021-10-13 17:43:43 +0300

    added letter H

--- alphabet/main.cpp
+++ alphabet/main.cpp
@@ -3,10 +3,20 @@
 
 using namespace std;
 
+void H(array<string, 6> &s)
+{
+    s[0] += " ||     | ";
+    s[1] += " ||     | ";
+    s[2] += " ||_____| ";
+    s[3] += " ||-----| ";
+    s[4] += " ||     | ";
+    s[5] += " ||     | ";
+}
 int main()
 {
     array<string, 6> screen;
-
+    H(screen);
+    H(screen);
     for (int i=0; i<6; i++)
         cout << screen[i] << endl;
 
