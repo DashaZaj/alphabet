818d865fee499708ad75ec3b906fcf700d0c972b
Dasha_Zajc <no@no.no>
2021-10-13 17:31:21 +0300

    added letter D

--- alphabet/main.cpp
+++ alphabet/main.cpp
@@ -3,12 +3,21 @@
 
 using namespace std;
 
+void D(array<string, 6> &s)
+{
+  s[0] += " ____ ";
+  s[1] += "|   \\\\";
+  s[2] += "|   ||";
+  s[3] += "|   ||";
+  s[4] += "|___//";
+  s[5] += "      ";
+}
+
 int main()
 {
     array<string, 6> screen;
-
+    D(screen);
     for (int i=0; i<6; i++)
         cout << screen[i] << endl;
-
     return 0;
 }
