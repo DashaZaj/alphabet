81e9729a282da127c43fa3776edc575f867eadf3
Dasha_Zajc <no@no.no>
2021-10-15 16:29:58 +0300

    v, r and z are merged

--- alphabet/main.cpp
+++ alphabet/main.cpp
@@ -35,12 +35,12 @@ void c(array<string, 6> &s)
 
 void D(array<string, 6> &s)
 {
-  s[0] += "|=======\\\\  ";
-  s[1] += "|        || ";
-  s[2] += "|        || ";
-  s[3] += "|        || ";
-  s[4] += "|        || ";
-  s[5] += "|-------//  ";
+  s[0] += "|======\\\\  ";
+  s[1] += "|       || ";
+  s[2] += "|       || ";
+  s[3] += "|       || ";
+  s[4] += "|       || ";
+  s[5] += "|------//  ";
 }
 
 void H(array<string, 6> &s)
@@ -53,7 +53,38 @@ void H(array<string, 6> &s)
     s[5] += " ||     | ";
 }
 
+void r(array<string, 6> &s)
+{
+    s[0] += " ||===\\\\ ";
+    s[1] += " ||   || ";
+    s[2] += " ||___// ";
+    s[3] += " ||\\\\    ";
+    s[4] += " || \\\\   ";
+    s[5] += " ||  \\\\  ";
+}
+
+void v(array<string, 6> &s)
+{
+    s[0] += "\\          //";
+    s[1] += " \\        // ";
+    s[2] += "  \\      //  ";
+    s[3] += "   \\    //   ";
+    s[4] += "    \\  //    ";
+    s[5] += "     \\//     ";
+}
+
+void z(array<string, 6> &s)
+{
+    s[0] += " ______ ";
+    s[1] += "     // ";
+    s[2] += "    //  ";
+    s[3] += "   //   ";
+    s[4] += "  //    ";
+    s[5] += " ______ ";
+}
+
 int main()
+
 {
     array<string, 6> screen;
     a(screen);
@@ -61,6 +92,9 @@ int main()
     c(screen);
     D(screen);
     H(screen);
+    r(screen);
+    v(screen);
+    z(screen);
     for (int i=0; i<6; i++)
         cout << screen[i] << endl;
     return 0;
