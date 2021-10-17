1c717eeb263416cba7985b4501fc8190955bf05b
Victoria <doodo@gmail.com>
2021-10-17 13:06:16 +0300

    added E

--- alphabet/main.cpp
+++ alphabet/main.cpp
@@ -3,10 +3,21 @@
 
 using namespace std;
 
+void e(array<string, 6> &s)
+{
+    s[0] += " ______ ";
+    s[1] += "||      ";
+    s[2] += "||      ";
+    s[3] += "||___   ";
+    s[4] += "||      ";
+    s[5] += "||_____ ";
+
+}
+
 int main()
 {
     array<string, 6> screen;
-
+    e(screen);
     for (int i=0; i<6; i++)
         cout << screen[i] << endl;
 
