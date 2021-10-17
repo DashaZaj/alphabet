05c7f415a8ab928451a521d0b882840c4ef1574c
Victoria <doodo@gmail.com>
2021-10-17 17:41:43 +0300

    added Q

--- alphabet/main.cpp
+++ alphabet/main.cpp
@@ -3,10 +3,20 @@
 
 using namespace std;
 
+void q(array < string, 6> &s)
+{ 
+    s[0] += "   _____     ";
+    s[1] += "  ||   ||    ";
+    s[2] += "  ||   ||    ";
+    s[3] += "  ||   ||    ";
+    s[4] += "  ||   ||    ";
+    s[5] += "   ----\\\\    ";
+}
 int main()
 {
     array<string, 6> screen;
-
+    q(screen);
+    q(screen);
     for (int i=0; i<6; i++)
         cout << screen[i] << endl;
 
