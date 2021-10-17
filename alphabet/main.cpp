25b5c65e30366f6ba47e2222f3d90ea98695268b
AnnaBoard <boardgame548@gmail.com>
2021-10-17 16:00:07 +0300

    added letter o

--- alphabet/main.cpp
+++ alphabet/main.cpp
@@ -3,12 +3,25 @@
 
 using namespace std;
 
+void o(array<string, 6> &s )
+
+{
+  s[0] +="      ====      ";
+  s[1] +="    ||    ||    ";
+  s[2] +="    ||    ||    ";
+  s[3] +="    ||    ||    ";
+  s[4] +="    ||    ||    ";
+  s[5] +="      ====      ";
+}
+
 int main()
 {
     array<string, 6> screen;
+    o(screen);
+    o(screen);
 
     for (int i=0; i<6; i++)
+    {
         cout << screen[i] << endl;
-
-    return 0;
+    }
 }
