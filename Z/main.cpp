be3e4be9f0b38d1b19a2f3dc6dfcd7c50cf70d02
Мирослава Жиздюк <no@no.no>
2021-10-17 15:33:08 +0300

    Added letter Z

---
+++ Z/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+void z(array<string, 6> &s)
+{
+    s[0] += "______";
+    s[1] += "    //";
+    s[2] += "   // ";
+    s[3] += "  //  ";
+    s[4] += " //   ";
+    s[5] += "______";
+}
+
+int main()
+{
+    array<string, 6> screen;
+    z(screen);
+    for (int i=0; i<6; i++)
+        cout << screen[i] << endl;
+
+    return 0;
+}
