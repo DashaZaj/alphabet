460c4e8935aede981e217bba0ff054ea0561cc95
Мирослава Жиздюк <no@no.no>
2021-10-17 17:06:58 +0300

    Added letter J

--- alphabet/main.cpp
+++ alphabet/main.cpp
@@ -3,10 +3,20 @@
 
 using namespace std;
 
+void j(array<string, 6> &s)
+{
+    s[0] += "    00";
+    s[1] += "    ||";
+    s[2] += "    ||";
+    s[3] += "    ||";
+    s[4] += "    //";
+    s[5] += "|__// ";
+}
+
 int main()
 {
     array<string, 6> screen;
-
+    j(screen);
     for (int i=0; i<6; i++)
         cout << screen[i] << endl;
 
